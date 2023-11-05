7235149aef54edb282e53132ee10e1853003f2ff
idr-yous <idr.yousfi@gmail.com>
2023-11-05 22:11:58 +0100

    project_6 - adding header

--- project_6_kasa/src/components/header/Header.jsx
+++ project_6_kasa/src/components/header/Header.jsx
@@ -1,5 +1,29 @@
 import "./Header.scss";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="header">
+      <div className="navbar container">
+        <NavLink to="/">
+          <img className="logo" src={logo} alt="kasa logo" />
+        </NavLink>
+        <nav className="menu">
+          <ul>
+            <li>
+              <NavLink className="nav-link" to="/">
+                Accueil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/about">
+                A propos
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
