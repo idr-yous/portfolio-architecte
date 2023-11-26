829039a672a4ba41ea619daf5f8ebaa5fad5fbbd
idr-yous <idr.yousfi@gmail.com>
2023-11-26 13:52:11 +0100

    project_6 - add error page

--- project_6_kasa/src/pages/error/Error.jsx
+++ project_6_kasa/src/pages/error/Error.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
 import "./Error.scss";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function Error() {
   return (
-    <div className="container">
-      <h1>Error page</h1>
-    </div>
+    <>
+      <Header />
+      <main className="main-content">
+        <div className="container">
+          <div className="error">
+            <h1 className="error_title">404</h1>
+            <h2 className="error_subtitle">
+              Oups! La page que vous demandez n'existe pas.
+            </h2>
+            <Link className="error_link" to="/">
+              Retourner sur la page d'accueil
+            </Link>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+    </>
   );
 }
